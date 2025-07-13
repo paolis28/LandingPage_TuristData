f0d4616374cfa3afa552b3fe72a6b5be9ad67333
paolis28 <shinge@outlook.es>
2025-07-13 04:24:40 -0600

    Implementación del logo favicon

---
+++ src/views/panelAdmin.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function PanelAdmin() {
+    return (
+        <div>
+        <h1>Panel de Administración</h1>
+        <p>Bienvenido al panel de administración.</p>
+        {/* Aquí puedes agregar más componentes o lógica para el panel de administración */}
+        </div>
+    );
+}
