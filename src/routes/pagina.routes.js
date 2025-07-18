92b225e62ce51abb0237cb58e8341bd0677e5f28
cesar <aguila622147@gmail.com>
2025-07-17 20:33:05 -0600

    Correcion de Rutas

--- src/routes/pagina.routes.js
+++ src/routes/pagina.routes.js
@@ -3,22 +3,21 @@ import Inicio from '../views/Inicio';
 import Register from '../views/Register';
 import Login from '../views/Login_admin';
 import Contacto from '../views/Contacto';
-import PanelAdmin from '../views/PanelAdmin';
+import PanelAdmin from '../views/panelAdmin';
 import PanelEstablecimiento from '../views/panel_establecimiento';
 import AvisoPrivacidad from '../views/avisoprivacidad';
-const Rutas =() =>{
-  return (
 
-      <Routes>
-        <Route path="/" element={<AvisoPrivacidad />} />
-        <Route path="/Inicio" element={<Inicio />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/contacto" element={<Contacto />} />
-        <Route path="/dashboard" element={<PanelAdmin />} />
-        <Route path="/dashestablecimiento" element={<PanelEstablecimiento />} />
-      </Routes>
- 
+const Rutas = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<AvisoPrivacidad />} />
+      <Route path="/Inicio" element={<Inicio />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/contacto" element={<Contacto />} />
+      <Route path="/panel-admin" element={<PanelAdmin />} />   
+      <Route path="/dashestablecimiento" element={<PanelEstablecimiento />} />
+    </Routes>
   );
 }
 
