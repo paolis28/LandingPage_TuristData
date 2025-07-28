26414a8a2178714c5df2918ab5cda48e15f924b7
paolis28 <shinge@outlook.es>
2025-07-28 14:16:37 -0600

    Añadiendo los  estados de México

--- src/views/Panel_eventosespeciales.js
+++ src/views/Panel_eventosespeciales.js
@@ -185,16 +185,46 @@ const handleRegistrarEvento = async () => {
 
           <div className="form-group">
             <label>Estado afectado:</label>
-            <select 
-              value={estadoAfectado} 
-              onChange={(e) => setEstadoAfectado(e.target.value)}
-              // disabled={!temporadaRegistrada || !lugarRegistrado}
-            >
-              <option value="">Selecciona el estado</option>
-              <option value="Chiapas">Chiapas</option>
-              <option value="Oaxaca">Oaxaca</option>
-              <option value="Yucatán">Yucatán</option>
-            </select>
+              <select 
+                value={estadoAfectado} 
+                onChange={(e) => setEstadoAfectado(e.target.value)}
+                // disabled={!temporadaRegistrada || !lugarRegistrado}
+              >
+                <option value="">Selecciona el estado</option>
+                <option value="Aguascalientes">Aguascalientes</option>
+                <option value="Baja California">Baja California</option>
+                <option value="Baja California Sur">Baja California Sur</option>
+                <option value="Campeche">Campeche</option>
+                <option value="Chiapas">Chiapas</option>
+                <option value="Chihuahua">Chihuahua</option>
+                <option value="Ciudad de México">Ciudad de México</option>
+                <option value="Coahuila">Coahuila</option>
+                <option value="Colima">Colima</option>
+                <option value="Durango">Durango</option>
+                <option value="Estado de México">Estado de México</option>
+                <option value="Guanajuato">Guanajuato</option>
+                <option value="Guerrero">Guerrero</option>
+                <option value="Hidalgo">Hidalgo</option>
+                <option value="Jalisco">Jalisco</option>
+                <option value="Michoacán">Michoacán</option>
+                <option value="Morelos">Morelos</option>
+                <option value="Nayarit">Nayarit</option>
+                <option value="Nuevo León">Nuevo León</option>
+                <option value="Oaxaca">Oaxaca</option>
+                <option value="Puebla">Puebla</option>
+                <option value="Querétaro">Querétaro</option>
+                <option value="Quintana Roo">Quintana Roo</option>
+                <option value="San Luis Potosí">San Luis Potosí</option>
+                <option value="Sinaloa">Sinaloa</option>
+                <option value="Sonora">Sonora</option>
+                <option value="Tabasco">Tabasco</option>
+                <option value="Tamaulipas">Tamaulipas</option>
+                <option value="Tlaxcala">Tlaxcala</option>
+                <option value="Veracruz">Veracruz</option>
+                <option value="Yucatán">Yucatán</option>
+                <option value="Zacatecas">Zacatecas</option>
+              </select>
+
           </div>
 
           <div className="button-group">
